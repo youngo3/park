fc1a8fce88d3f9c301282579f8e6595e18142596
youngo3 <58507488+youngo3@users.noreply.github.com>
2020-07-11 02:24:32 +0900

    Fix App.js
    change structure

--- src/App.js
+++ src/App.js
@@ -1,39 +1,5 @@
-gsap.registerPlugin(ScrollTrigger);
+import intro from "./Intro.js";
+import parkRender from "./ParkRender.js";
 
-ScrollTrigger.defaults({
-  toggleActions: "restart pause reverse pause",
-});
-
-let tl = gsap.timeline({
-  scrollTrigger: {
-    trigger: ".intro",
-    end: "+=100%",
-    pin: true,
-    scrub: 1,
-    mark: true,
-  },
-});
-tl.to(".intro", {
-  filter: `grayScale(0)`,
-  // onComplete: () => console.log(window.innerHeight),
-});
-tl.to(".muhly", {
-  y: "+=700",
-});
-tl.to(".cloud", {
-  x: "+=700",
-});
-
-gsap.to(".park-model img", {
-  scrollTrigger: {
-    trigger: ".park-model",
-    start: "top",
-    pin: true,
-    scrub: 1,
-    markers: {
-      startColor: "white",
-      endColor: "green",
-    },
-  },
-  rotate: 30,
-});
+intro();
+parkRender();
